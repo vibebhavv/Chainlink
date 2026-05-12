e9b50b18f40e787637767ea57f06f9da50a27428
Vaibhav <167416173+vibebhavv@users.noreply.github.com>
2026-05-12 14:32:09 +0530

    Add known exchanges to wallet_tags.py

---
+++ backend/utils/wallet_tags.py
@@ -0,0 +1,29 @@
+KNOWN_EXCHANGES = {
+
+    # Binance
+    "34xp4vRoCGJym3xR7yCVPFHoCNxv4Twseo":
+        "Binance",
+
+    "1NDyJtNTjmwk5xPNhjgAMu4HDHigtobu1s":
+        "Binance",
+
+    # Coinbase
+    "3CDjnCBVQm2fTj6GXSGjbeMM9VE6H7A66W":
+        "Coinbase",
+
+    # Kraken
+    "bc1q8gn5fuu2eva2cw3z6rsusj6wzstu7e9cw4a6p3":
+        "Kraken",
+
+    # Bitfinex
+    "3D2oetdNuZUqQHPJmcMDDHYoqkyNVsFk9r":
+        "Bitfinex",
+
+    # Huobi
+    "1HT7xU2Ngenf7D4yocz2SAcnNLW7rK8d4E":
+        "Huobi",
+
+    # OKX
+    "3M219KR5vEneNb47ewrPfWyb5jQ2DjxRP6":
+        "OKX",
+}
